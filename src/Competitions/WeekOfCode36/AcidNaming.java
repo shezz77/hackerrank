3c8c8bec46eb6944916a8e4b7cda5b4bf64b9d56
Shehzad Aslam <shehzada.pk786@gmail.com>
2018-02-05 21:53:12 +0500

    Week of Code 36 - Q1 Solved

--- src/Competitions/WeekOfCode36/AcidNaming.java
+++ src/Competitions/WeekOfCode36/AcidNaming.java
@@ -8,9 +8,34 @@ import java.util.Scanner;
  * Created by SA on 05-Feb-18.
  */
 public class AcidNaming {
+    private static String CHECK_STRING1 = "hydro";
+    private static String CHECK_STRING2 = "ic";
 
     private static String acidNaming(String acid_name) {
-        String ans = "";
+        boolean firstCheck = true;
+        boolean secondCheck = true;
+        String ans = "not an acid";
+
+        for (int i = 0; i<5; i++){
+            if (acid_name.charAt(i) != CHECK_STRING1.charAt(i)){
+                firstCheck = false;
+                break;
+            }
+        }
+
+        int len = acid_name.length()-2;
+        for (int i = 0; i<2; i++){
+            if (CHECK_STRING2.charAt(i) != acid_name.charAt(len+i)){
+                secondCheck = false;
+                break;
+            }
+        }
+
+        if (firstCheck && secondCheck){
+            ans = "non-metal acid";
+        }else if (secondCheck) {
+            ans = "polyatomic acid";
+        }
 
         return ans;
     }
